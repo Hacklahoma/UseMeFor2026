ef1872adebfb602a373fbfbe78218d1575f7c596
Nhu Van <nhu.n.van-1@ou.edu>
2026-02-07 09:18:30 -0600

    Change American Fidelity Workshop name

--- frontend/src/ui/pages/faq/index.tsx
+++ frontend/src/ui/pages/faq/index.tsx
@@ -171,7 +171,7 @@ const FAQPage: React.FC = () => {
                       <p><span className="font-bold">11:30 AM (CST)</span> - Opening Ceremony</p>
                       <p><span className="font-bold">12:00 PM (CST)</span> - Hacking Begins!</p>
                       <p><span className="font-bold">1:30 PM (CST)</span> - Lunch - Abu Omar</p>
-                      <p><span className="font-bold">2:30 PM (CST)</span> - American Fidelity Workshop: AI Voice Incorporation</p>
+                      <p><span className="font-bold">2:30 PM (CST)</span> - American Fidelity Workshop: AI Agents</p>
                       <p><span className="font-bold">3:30 PM (CST)</span> - AI Agents & Vibe Engineering Workshop with Fazil Raja</p>
                       <p><span className="font-bold">4:00 PM (CST)</span> - Snack Time</p>
                       <p><span className="font-bold">4:30 PM (CST)</span> - Innovation Hub Workshop: Turn your project into startup</p>
